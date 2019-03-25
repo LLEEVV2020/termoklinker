affc087f4aa3e8023411596a6a6c83d7959d596a
LLEEVV2020 <lleevv2020@gmail.com>
2019-03-26 02:50:06 +0400

    add

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,32 @@
 "use strict";
 
-var g = 25;
+document.querySelectorAll('.swiper-container').forEach(function (elem) {
+  var elemWrap = elem.parentElement.nextElementSibling;
+  new Swiper(elem, {
+    slidesPerView: 4,
+    spaceBetween: 25,
+    loop: true,
+    navigation: {
+      nextEl: elemWrap.querySelectorAll('.swiper-next'),
+      prevEl: elemWrap.querySelectorAll('.swiper-prev')
+    },
+    breakpoints: {
+      1180: {
+        slidesPerView: 3,
+        spaceBetween: 25
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 0
+      },
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 0
+      }
+    }
+  });
+});

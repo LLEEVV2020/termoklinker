0a397f81babc0aed920137a232d98e23a4e37ad5
LLEEVV2020 <lleevv2020@gmail.com>
2019-03-28 19:56:45 +0400

    add

--- app/babel/main.js
+++ app/babel/main.js
@@ -76,9 +76,9 @@ $(".img-1").hover(
     }
     
 );
-$(".content-1").click(
+$(".content-1 iframe").mouseup(
     function () {
-        console.log("uuuuu");
+        console.log("https://ru.stackoverflow.com/questions/520943/%D0%9E%D1%82%D1%81%D0%BB%D0%B5%D0%B4%D0%B8%D1%82%D1%8C-%D0%BD%D0%B0%D0%B6%D0%B0%D1%82%D0%B8%D1%8F-play-%D0%BF%D0%BE-%D0%B2%D0%B8%D0%B4%D0%B5%D0%BE-%D1%81-%D1%8E%D1%82%D1%83%D0%B1%D0%B0-%D0%B2-iframe");
     }
 );
 
--- app/js/main.js
+++ app/js/main.js
@@ -76,8 +76,8 @@ $(".img-1").hover(function () {
     "display": "block"
   });
 });
-$(".content-1").click(function () {
-  console.log("uuuuu");
+$(".content-1 iframe").mouseup(function () {
+  console.log("https://ru.stackoverflow.com/questions/520943/%D0%9E%D1%82%D1%81%D0%BB%D0%B5%D0%B4%D0%B8%D1%82%D1%8C-%D0%BD%D0%B0%D0%B6%D0%B0%D1%82%D0%B8%D1%8F-play-%D0%BF%D0%BE-%D0%B2%D0%B8%D0%B4%D0%B5%D0%BE-%D1%81-%D1%8E%D1%82%D1%83%D0%B1%D0%B0-%D0%B2-iframe");
 });
 $(".img-2").hover(function () {
   $(".js-content > *").css({

cf024088becf592112389a72f373c4bb442846b0
LLEEVV2020 <lleevv2020@gmail.com>
2019-04-04 16:58:38 +0400

    add

--- app/babel/main.js
+++ app/babel/main.js
@@ -210,3 +210,9 @@ $(".img-5").hover(
 );
 
 
+$(window).on('load', function () {
+  var $preloader = $('#p_prldr'),
+      $svg_anm   = $preloader.find('.svg_anm');
+  $svg_anm.fadeOut();
+  $preloader.delay(500).fadeOut('slow');
+});
